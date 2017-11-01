54a2e4ebccf43fd80f8b0196632af26e76d21ec5
Liam Blythe <l.blythe@sig.eu>
2017-11-01 09:11:29 +0100

    fixed missing implementation in unit test exercise

--- src/eu/sig/safari/exercises/automate_tests/banking/stubs/Money.java
+++ src/eu/sig/safari/exercises/automate_tests/banking/stubs/Money.java
@@ -2,9 +2,14 @@ package eu.sig.safari.exercises.automate_tests.banking.stubs;
 
 public class Money {
 
-	public boolean greaterThan(int transferLimit) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    private int amount;
+
+    public Money(int amount) {
+        this.amount = amount;
+    }
+
+    public boolean greaterThan(int otherAmount) {
+        return this.amount > otherAmount;
+    }
 
 }
